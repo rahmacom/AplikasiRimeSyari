5c23ab3592adde472b9d4690b6e7b56e95b1bcda
rahmacom <rahmanrahma30@gmail.com>
2021-07-10 17:00:38 +0700

    step1

---
+++ app/src/main/java/com/rahmacom/rimesyarifix/ui/belanja/BelanjaFragment.java
@@ -0,0 +1,38 @@
+package com.rahmacom.rimesyarifix.ui.belanja;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rahmacom.rimesyarifix.R;
+
+public class BelanjaFragment extends Fragment {
+
+    private BelanjaViewModel mViewModel;
+
+    public static BelanjaFragment newInstance() {
+        return new BelanjaFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.belanja_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(BelanjaViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/rahmacom/rimesyarifix/ui/belanja/BelanjaViewModel.java
@@ -0,0 +1,7 @@
+package com.rahmacom.rimesyarifix.ui.belanja;
+
+import androidx.lifecycle.ViewModel;
+
+public class BelanjaViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
