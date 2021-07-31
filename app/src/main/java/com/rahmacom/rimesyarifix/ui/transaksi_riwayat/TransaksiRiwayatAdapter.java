887a77789ff67a0342250ce3c60b27b051cde1ad
rahmacom <rahmanrahma30@gmail.com>
2021-07-31 11:40:33 +0700

    penambahan data palsu transaksi

---
+++ app/src/main/java/com/rahmacom/rimesyarifix/ui/transaksi_riwayat/TransaksiRiwayatAdapter.java
@@ -0,0 +1,61 @@
+package com.rahmacom.rimesyarifix.ui.transaksi_riwayat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.rahmacom.rimesyarifix.databinding.ItemDataProfilBinding;
+import com.rahmacom.rimesyarifix.databinding.ItemTransaksiRiwayatBinding;
+import com.rahmacom.rimesyarifix.ui.profil.DataProfilAdapter;
+import com.rahmacom.rimesyarifix.ui.profil.Profil;
+
+import java.util.ArrayList;
+
+public class TransaksiRiwayatAdapter extends RecyclerView.Adapter<TransaksiRiwayatAdapter.ViewHolder> {
+    private ItemTransaksiRiwayatBinding binding;
+    private ArrayList<Transaksi> listData = new ArrayList<>();
+
+    public void setLists(ArrayList<Transaksi> list) {
+        listData.clear();
+        listData.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public TransaksiRiwayatAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding = ItemTransaksiRiwayatBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private ItemTransaksiRiwayatBinding binding;
+
+        ViewHolder(@NonNull ItemTransaksiRiwayatBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        void bind(Transaksi transaksi) {
+            binding.tvItemNoTransaksi.setText(transaksi.getNoTransaksi());
+        }
+    }
+}
--- app/src/main/java/com/rahmacom/rimesyarifix/ui/transaksi_riwayat/TransaksiRiwayatFragment.java
+++ app/src/main/java/com/rahmacom/rimesyarifix/ui/transaksi_riwayat/TransaksiRiwayatFragment.java
@@ -9,8 +9,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import com.rahmacom.rimesyarifix.R;
 import com.rahmacom.rimesyarifix.databinding.FragmentTransaksiRiwayatBinding;
+import com.rahmacom.rimesyarifix.ui.home.DataProdukAdapter;
+import com.rahmacom.rimesyarifix.ui.home.Produk;
+
+import java.util.ArrayList;
 
 public class TransaksiRiwayatFragment extends Fragment {
 
@@ -34,12 +40,40 @@ public class TransaksiRiwayatFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.d("RiwayatTransaksiFragment", "This is riwayat transaksi fragment");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private ArrayList<Transaksi> getListTransaksi() {
+        String[] noTransaksi = getResources().getStringArray(R.array.no_transaksi);
+        String[] tanggal = getResources().getStringArray(R.array.tanggal_transaksi);
+        String[] jumlah = getResources().getStringArray(R.array.jumlah_transaksi);
+        String[] total = getResources().getStringArray(R.array.total_harga);
+
+        ArrayList<Transaksi> list = new ArrayList<>();
+
+        for (int i = 0; i < noTransaksi.length; i++) {
+            Transaksi transaksi = new Transaksi();
+            transaksi.setNoTransaksi(noTransaksi[i]);
+            transaksi.setTanggal(tanggal[i]);
+            transaksi.setJumlah(jumlah[i]);
+            transaksi.setTotal(total[i]);
+            list.add(transaksi);
+        }
+        return list;
+    }
+
+    private void setUpDataProduk() {
+        ArrayList<Transaksi> listTransaksi = getListTransaksi();
+        TransaksiRiwayatAdapter adapter = new TransaksiRiwayatAdapter();
+        adapter.setLists(listTransaksi);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+
+        binding.rvFragmentTransaksiRiwayat.setAdapter(adapter);
+        binding.rvFragmentTransaksiRiwayat.setLayoutManager(gridLayoutManager);
     }
 }
