5d870117ba4a63f27f745b123ae08bf8e00ee7cb
Rizky92 <myquinn489@gmail.com>
2021-08-23 14:32:07 +0800

    Update

--- app/src/main/java/com/rahmacom/rimesyarifix/ui/profil_biodata_alamat/ProfilBiodataAlamatFragment.java
+++ app/src/main/java/com/rahmacom/rimesyarifix/ui/profil_biodata_alamat/ProfilBiodataAlamatFragment.java
@@ -1,4 +1,4 @@
-package com.rahmacom.rimesyarifix.ui.keranjang_checkout;
+package com.rahmacom.rimesyarifix.ui.profil_biodata_alamat;
 
 import androidx.lifecycle.ViewModelProvider;
 
@@ -7,21 +7,25 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.rahmacom.rimesyarifix.R;
-import com.rahmacom.rimesyarifix.databinding.FragmentKeranjangCheckoutBinding;
+import com.rahmacom.rimesyarifix.databinding.FragmentProfilBiodataAlamatBinding;
+import com.rahmacom.rimesyarifix.manager.PreferenceManager;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class KeranjangCheckoutFragment extends Fragment {
+public class ProfilBiodataAlamatFragment extends Fragment {
 
-    private KeranjangCheckoutViewModel viewModel;
-    private FragmentKeranjangCheckoutBinding binding;
+    private FragmentProfilBiodataAlamatBinding binding;
+    private ProfilBiodataAlamatViewModel viewModel;
+    private PreferenceManager manager;
+    private NavController navController;
 
     @Override
     public View onCreateView(
@@ -29,12 +33,7 @@ public class KeranjangCheckoutFragment extends Fragment {
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState
     ) {
-        binding = FragmentKeranjangCheckoutBinding.inflate(inflater, container, false);
+        binding = FragmentProfilBiodataAlamatBinding.inflate(inflater, container, false);
         return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        viewModel = new ViewModelProvider(requireActivity()).get(KeranjangCheckoutViewModel.class);
     }
 }
